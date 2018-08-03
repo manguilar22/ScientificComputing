db658d85f89337215827b26aa8760fe0ad033f45
user <manguilar22@gmail.com>
2018-08-03 00:37:29 -0600

    Fri Aug  3 00:37:29 MDT 2018

--- topology/Torus.py
+++ topology/Torus.py
@@ -35,15 +35,24 @@ fig3 =FF.create_trisurf(x=x,y=y,z=z,simplices=simplices,
                         title="Torus V. 3",
                         colormap="RdBu",
                         aspectratio=dict(x=.2,y=.2,z=1))
+fig4=go.Scatter3d(x=x,y=y,z=z,mode="markers",marker=dict(size=.5,symbol="square",colorscale="polar"))
 
-layout=go.Layout(yaxis=dict(title="Y-axis"),
+trace01=go.Scatter(x=x,y=z,mode="markers",name="(X-Axis,Z-Axis)",marker=dict(size=.5,symbol="circle-open"))
+trace02=go.Scatter(x=x,y=y,mode="lines",name="(X-Axis,Y-Axis)",marker=dict(size=.3))
+
+layout2D=go.Layout(title="Scatter-2D Torus",yaxis=dict(title="Y-axis"),
+                 xaxis=dict(title="X-axis"),hovermode="closest")
+
+layout=go.Layout(title="Scatter-3D Torus",yaxis=dict(title="Y-axis"),
                  xaxis=dict(title="X-axis"),hovermode="closest")
 
 app.layout = html.Div([
     dcc.Markdown("""# Torus"""),
     dcc.Graph(id="Torus-Graph",figure=dict(data=fig,layout=layout)),
+    dcc.Graph(id="Torus-Scatter",figure=dict(data=[fig4],layout=layout)),
     html.H2("Version 2"),
     dcc.Graph(id="Torus-V2",figure=dict(data=fig2)),
+    dcc.Graph(id="Torus-2D",figure=dict(data=[trace01,trace02],layout=layout2D)),
     html.H2("Version 3"),
     dcc.Graph(id="Torus-V3",figure=dict(data=fig3))
 ])
